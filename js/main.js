9508af60196cad79c65a444de7dac759f123f341
rabea <julianqrooz@gmail.com>
2025-06-12 15:40:06 +0300

    update oninput instead onkeyup

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ let textar = document.getElementById("textarea");
 let span = document.getElementById("myspan");
 let spantwo = document.querySelector(".note");
 
-textar.onkeyup = function () {
+textar.oninput = function () {
   span.textContent = 50 - this.value.length;
 
-  if (span.textContent < 0) {
+  if (span.innerHTML == 0) {
     span.style.color = "#f00";
-    spantwo.textContent = "You have exceeded the word limit";
+    spantwo.textContent = "You have finished the allowed number of words";
   } else {
     span.style.color = "#000";
     spantwo.textContent = "";
